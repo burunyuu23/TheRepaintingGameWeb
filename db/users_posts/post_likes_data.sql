f04b7365dc0273ae266586411c60c7ae9b82c123
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2023-08-14 15:03:11 +0300

    fix: change db scheme and test data set.
    - Cnage relation between user and posts from 'many-to-many' to 'one-to-many'.
    - Update users id in data set from one digit to 36 uuid string for example from 1 to '11111111-1111-1111-1111111111111111'.
    - Move tad_data to user_posts dir.
    - Add auto generation for comments and posts id.

--- db/users_posts/post_likes_data.sql
+++ db/users_posts/post_likes_data.sql
@@ -3,23 +3,23 @@ INSERT INTO post_likes (post_id,
                            user_id
 )
 SELECT 1,
-       '1'
+       '11111111-1111-1111-1111111111111111'
 WHERE NOT EXISTS (SELECT post_id,user_id
                   FROM post_likes
-                  WHERE post_id = 1 AND user_id = '1');
+                  WHERE post_id = 1 AND user_id = '11111111-1111-1111-1111111111111111');
 INSERT INTO post_likes (post_id,
                         user_id
 )
 SELECT 1,
-       '2'
+       '22222222-2222-2222-2222222222222222'
 WHERE NOT EXISTS (SELECT post_id,user_id
                   FROM post_likes
-                  WHERE post_id = 1 AND user_id = '2');
+                  WHERE post_id = 1 AND user_id = '22222222-2222-2222-2222222222222222');
 INSERT INTO post_likes (post_id,
                         user_id
 )
 SELECT 1,
-       '3'
+       '22222222-2222-2222-2222222222222222'
 WHERE NOT EXISTS (SELECT post_id,user_id
                   FROM post_likes
-                  WHERE post_id = 1 AND user_id = '3');
+                  WHERE post_id = 1 AND user_id = '22222222-2222-2222-2222222222222222');
